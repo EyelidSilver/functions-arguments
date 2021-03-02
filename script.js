2ca4b89d1c4a32564bf9e72dc475d89f99428ce6
Aita K <67955300+EyelidSilver@users.noreply.github.com>
2021-03-02 12:27:14 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+
+
+const paintWalls = (location, color) => {
+    console.log('The '+ location + ' wall has been painted ' + color)
+}
+
+paintWalls()
+paintWalls("north","green")
+paintWalls("south-east","blue")
+
+paintWalls("blue", "south-east")
